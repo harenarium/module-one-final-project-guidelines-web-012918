7122ea11b2325980a4bdf4936538d13a6165c8ac
harenarium <22226614+harenarium@users.noreply.github.com>
2018-02-12 17:41:12 -0500

    inital migrate fixed typos

--- db/migrate/20180212211423_create_words.rb
+++ db/migrate/20180212211423_create_words.rb
@@ -1,8 +1,8 @@
 class CreateWords < ActiveRecord::Migration[5.0]
   def change
     create_table :words do |t|
-      t.string word
-      t.string clue
+      t.string :word
+      t.string :clue
     end
   end
 end
